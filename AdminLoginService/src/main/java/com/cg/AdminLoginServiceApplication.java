80425a0e154d8f2af7106e83580aa135d11f0e88
EarlapatiSahithi <62692842+EarlapatiSahithi@users.noreply.github.com>
2020-08-31 15:52:46 +0530

    Commiting

---
+++ AdminLoginService/src/main/java/com/cg/AdminLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminLoginServiceApplication.class, args);
+	}
+
+}
